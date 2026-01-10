7792371203fdd385807b494d7c6d7db91fa234fb
Riku Yukimaru <setsugan.robocon@gmail.com>
2026-01-11 00:34:15 +0900

    feature: frameあたりの描画数を設定できるようにし、高速化

--- montecarlo.cpp
+++ montecarlo.cpp
@@ -16,8 +16,10 @@ int main()
 
     constexpr double RADIUS { WIDTH / 2.0 };
 
+    constexpr size_t POINTS_PER_FRAME {10000};
+
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "montecarlo");
-    window.setFramerateLimit(500);
+    window.setFramerateLimit(60);
 
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -41,23 +43,26 @@ int main()
             }
         }
 
-        Point point {dist(gen), dist(gen)};
-        bool is_inside {false};
-
-        if (pow(point.x, 2.0) + pow(point.y, 2.0) <= pow(RADIUS, 2.0))
+        for (size_t i{}; i < POINTS_PER_FRAME; ++i)
         {
-            is_inside = true;
-            ++inside_points;
+            Point point {dist(gen), dist(gen)};
+            bool is_inside {false};
+
+            if (point.x*point.x + point.y*point.y <= RADIUS*RADIUS)
+            {
+                is_inside = true;
+                ++inside_points;
+            }
+            ++total_points;
+
+            sf::Color color {is_inside ? sf::Color::Green : sf::Color::Red};
+            points.append(sf::Vertex(sf::Vector2f(point.x + RADIUS, point.y + RADIUS), color));
         }
-        ++total_points;
-
-        sf::Color color {is_inside ? sf::Color::Green : sf::Color::Red};
-        points.append(sf::Vertex(sf::Vector2f(point.x + RADIUS, point.y + RADIUS), color));
-
+        
         pi = static_cast<double>(inside_points) / static_cast<double>(total_points) * 4.0;
-        if (total_points % 100 == 0)
+        if (total_points % 10000 == 0)
         {
-            std::cout << "pi : " << pi << std::endl;
+            std::cout <<"total : " << total_points << "points. pi : " << pi << std::endl;
         }
 
         window.clear(sf::Color::Black);
